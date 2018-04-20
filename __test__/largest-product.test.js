f21c306a38ac9628fadfe6c532ef28755d07ed45
Joshua Fredrickson <uafredrickson@gmail.com>
2018-04-19 20:27:26 -0700

    8:30pm code

--- __test__/largest-product.test.js
+++ __test__/largest-product.test.js
@@ -2,8 +2,14 @@
 
 const largestProduct = require('../largest-product.js');
 
-describe('Find the largest product from the array' () => {
-  test('Should return 5 from arrayZ[[1,1],[1,5],[1,1]]', () => {
-    expect(largestProduct(arrayZ[[1,1],[1,5],[1,1]])).toEqual(5);
+describe('Find the largestProduct from the array', () => {
+  test('Should return 5 from arrayS[[1,1],[1,5],[1,1]]', () => {
+    expect(largestProduct(arrayS[[1,1],[1,5],[1,1]])).toEqual(5);
+  });
+  test('Should return undefined from arrayS[[3,1],[Z,4],[8,9]]', () => {
+    expect(largestProduct(arrayS[[3,1],[Z,4],[8,9]])).toEqual(undefined);
+  });
+  test('Should return 81 from arrayS[[3,1,2],[5,2,9],[1,2,9]]', () => {
+    expect(largestProduct(arrayS[[3,1,2],[5,2,9],[1,2,9]])).toEqual(81);
   });
 });
--- largest-product.js
+++ largest-product.js
@@ -3,27 +3,26 @@
 
 const largestProduct = module.exports = {};
 
-let arrayZ = [[1,2,3], [2,3,1] [3,1,2]];
+let arrayS = [[1,2,3], [2,3,1], [3,1,2]];
 
 
-function largestProduct (arrayZ) {
+function largestProduct (arrayS) {
   let highestProduct = 0;
-  const outerLen = arrayZ.length;
-  // const innerLen = arrayZ[0].length;
-
+  const outerLen = arrayS.length;
+  // const innerLen = arrayS[0].length;
   for (let j = 0; j < outerLen; j++){
     if(j - 1 >= 0) {
       console.log('j', j);
-      for (let k = 0; k < arrayZ[j].length; k++) {
+      for (let k = 0; k < arrayS[j].length; k++) {
         if (k - 1 >= 0) {
           console.log('k', k);
-          let y = arrayZ[j][k] * arrayZ[j][k + 1];
+          let y = arrayS[j][k] * arrayS[j][k + 1];
           if (y > highestProduct) {
             highestProduct = y;
             console.log('y', y);
           }
           ;
-          let x = arrayZ[j][k] * arrayZ[j][k - 1];
+          let x = arrayS[j][k] * arrayS[j][k - 1];
           if (x > highestProduct) {
             highestProduct = x;
             console.log('x', x);
@@ -32,5 +31,6 @@ function largestProduct (arrayZ) {
       }
     }
   }
+  return highestProduct;
 }
 
