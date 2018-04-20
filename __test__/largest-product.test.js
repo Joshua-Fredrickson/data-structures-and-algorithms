99220e1fd9b15f6feca1f36a918cae13e232250a
Joshua Fredrickson <uafredrickson@gmail.com>
2018-04-19 19:36:32 -0700

    7:30pm commit...

---
+++ __test__/largest-product.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const largestProduct = require('../largest-product.js');
+
+describe('Find the largest product from the array' () => {
+  test('Should return 5 from arrayZ[[1,1],[1,5],[1,1]]', () => {
+    expect(largestProduct(arrayZ[[1,1],[1,5],[1,1]])).toEqual(5);
+  });
+});
