0cd7d102e4e9470066323eb707905c8dfb3966d8
Joshua Fredrickson <uafredrickson@gmail.com>
2018-04-25 21:55:30 -0700

    initial try

---
+++ __test__/ll-merge.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const mergeList = require ('../ll-merge');
+
+
+describe('Linked Lists merge tests', () => {
+  // test('#constructor', () => {
+  //   const testList = new LinkedList();
+  //   expect(testList.head).toBeNull();
+  // });
+
+//refactor!!!
