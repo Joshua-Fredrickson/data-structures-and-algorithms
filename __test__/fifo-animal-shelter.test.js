0a2c5fcb324697138192d220a66aff4d4f50bc2f
Joshua Fredrickson <uafredrickson@gmail.com>
2018-05-01 21:47:44 -0700

    passing 1 test

--- __test__/fifo-animal-shelter.test.js
+++ __test__/fifo-animal-shelter.test.js
@@ -1,8 +1,13 @@
 'use strict';
 
-describe('Test will take in a string of cat', () => {
-  test('testing for cat output', () => {
+describe('Test will take in a string of a cat or dog string', () => {
+  test('sucess - testing for cat output', () => {
     expect('cat').toEqual({ name: 'kitty', species: 'cat'});
   });
-  test('testing for empty input')
+  test('failure - testing for empty input', () => {
+    expect(' ').toBeNull;
+  });
+  test('failure - testing input of dog', () => {
+    expect('dog').toEqual({ name: 'bella', species: 'dog'});
+  })
 });
