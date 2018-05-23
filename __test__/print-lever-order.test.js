518d1cf718fb9682d4f5491c30439e2acf703ea6
Joshua Fredrickson <uafredrickson@gmail.com>
2018-05-23 08:48:39 -0700

    print-level-order

---
+++ __test__/print-lever-order.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Test the Print Level Order (aka pLO) function', () => {
+  test('sucess - testing for correct output', () => {
+    expect('kTree').toEqual({'a\nbc\nzdef');
+  });
+});
